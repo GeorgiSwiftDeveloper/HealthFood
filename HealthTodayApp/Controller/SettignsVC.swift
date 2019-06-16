b75573c05d2271a2cf06aa4780d8718c5926dfdc
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2019-06-16 11:25:50 -0700

    Working on SettingsVC TableView

--- HealthTodayApp/Controller/SettignsVC.swift
+++ HealthTodayApp/Controller/SettignsVC.swift
@@ -7,24 +7,42 @@
 //
 
 import UIKit
+import CoreData
+import Foundation
 
-class SettignsVC: UIViewController {
 
+class SettignsVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+  
+    
+
+    
+    
+    let settingsList = ["Your Goal","Smart Notification","Clear data","Follow on Instagram", "Follow on Facebook", "Follow on Twitter", "Support Developer","Team"]
+    let settingsImage = ["goal","notification","data","instagram","facebook","twitter","message","team"]
+    
+    
+    var coreDataModel = CoreDataStackClass()
+    var goal = [Goal]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? SettingsCell else {return UITableViewCell()}
+        return cell
+    }
 
 }
---
+++ TableVIewCell/SettingsCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsCell.swift
+//  HealthTodayApp
+//
+//  Created by Georgi Malkhasyan on 6/16/19.
+//  Copyright © 2019 Malkhasyan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
